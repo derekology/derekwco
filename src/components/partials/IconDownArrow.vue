646315a2318eadc0f319cb4adb04796807347899
Derek Woo <me@derekw.co>
2023-06-02 12:55:17 -0700

    Add down arrow icon

---
+++ src/components/partials/IconDownArrow.vue
@@ -0,0 +1,5 @@
+<template>
+    <svg width="24" height="24" fill="currentColor">
+        <path d="M23.245 4l-11.245 14.374-11.219-14.374-.781.619 12 15.381 12-15.391-.755-.609z" />
+    </svg>
+</template>
