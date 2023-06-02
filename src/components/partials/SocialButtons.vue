f64a1f9325ad5e0daee051382a3a91f92162f425
Derek Woo <me@derekw.co>
2023-06-02 12:55:57 -0700

    Add social buttons component

---
+++ src/components/partials/SocialButtons.vue
@@ -0,0 +1,37 @@
+<template>
+    <a href="https://github.com/derekology/" target="_blank" class="social-icon">
+        <GithubIcon />
+    </a>
+    <a href="https://linkedin.com/in/derekology/" target="_blank" class="social-icon">
+        <LinkedinIcon />
+    </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import GithubIcon from '@/components/partials/IconGithub.vue'
+import LinkedinIcon from '@/components/partials/IconLinkedin.vue'
+
+export default defineComponent({
+    name: 'SocialButtons',
+    components: {
+        LinkedinIcon,
+        GithubIcon,
+    },
+});
+
+</script>
+
+<style scoped>
+.social-icon {
+    color: var(--color-text);
+    margin-top: 0.1rem;
+    margin-bottom: 0.1rem;
+    margin-right: 10px;
+}
+
+.social-icon:hover {
+    color: var(--color-hover);
+    transition: color 0.2s ease-in-out;
+}
+</style>
