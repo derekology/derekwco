a8b24ef295cb477b8c3452e89bdc8e1fb478086a
Derek Woo <me@derekw.co>
2023-06-03 11:49:52 -0700

    Add Profile Certifications component

---
+++ src/components/partials/ProfileCertifications.vue
@@ -0,0 +1,12 @@
+<template>
+    <img id="cert-adobe-mce" src="/assets/cert-adobe-mce.png" alt="Picture of Derek Woo" title="Display Picture" />
+    <img id="cert-salesforce-mca" src="/assets/cert-salesforce-mca.png" alt="Picture of Derek Woo"
+        title="Display Picture" />
+</template>
+
+<style scoped>
+img {
+    height: 75px;
+    margin: 15px 25px;
+}
+</style>
