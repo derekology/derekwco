0303b063fdea2b7f1988e280256ad3c17341bc6a
Derek Woo <me@derekw.co>
2023-06-03 12:51:53 -0700

    Build layout for Skill section

--- src/components/sections/SkillSection.vue
+++ src/components/sections/SkillSection.vue
@@ -1,5 +1,23 @@
 <template>
-    <h1>Skills</h1>
+    <div id="skills-list">
+        <span class="section-header">Development</span>
+        <p>
+            HTML/CSS/JS/TS, EJS, Node.js, Express, Bootstrap, Vue, Google Firebase
+        </p>
+        <span class="section-header">Automation / Scripting</span>
+        <p>
+            Python, Javascript, Selenium, Twill, Bash, AMPScript, SSJS, Velocity
+        </p>
+        <span class="section-header">Databases</span>
+        <p>
+            SQL, Google Firestore, MongoDB, GraphQL
+        </p>
+        <span class="section-header">Platforms</span>
+        <p>
+            Adobe Marketo Engage, Salesforce Marketing / Sales Cloud, MailChimp, MailerLite, Shopify, Wix, Wordpress,
+            Manjaro, Arch, Ubuntu, Windows
+        </p>
+    </div>
 </template>
 
 <script lang="ts">
@@ -12,4 +30,16 @@ export default defineComponent({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+#skills-list {
+    display: flex;
+    flex-direction: column;
+    padding: 0 5%;
+}
+
+p {
+    margin-bottom: 3em;
+    font-size: 1.5rem;
+    line-height: 1.75em;
+}
+</style>
