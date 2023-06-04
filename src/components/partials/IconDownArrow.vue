0fd3173ddae270f4760e95b778506b6fe20cbcc4
Derek Woo <me@derekw.co>
2023-06-03 23:14:25 -0700

    Add component export for consistency

--- src/components/partials/IconDownArrow.vue
+++ src/components/partials/IconDownArrow.vue
@@ -3,3 +3,11 @@
         <path d="M23.245 4l-11.245 14.374-11.219-14.374-.781.619 12 15.381 12-15.391-.755-.609z" />
     </svg>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'DownArrowIcon',
+});
+</script>
