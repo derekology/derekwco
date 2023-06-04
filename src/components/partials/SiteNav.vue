6234f5c0480fb2ad66e7c90494e08c6e2caafba0
Derek Woo <me@derekw.co>
2023-06-04 11:28:31 -0700

    Add close menu upon click functionality

--- src/components/partials/SiteNav.vue
+++ src/components/partials/SiteNav.vue
@@ -106,6 +106,7 @@ export default defineComponent({
              * @param {string} desiredSection - The section to scroll to
              */
             ScrollToSectionMixin.methods?.scrollToSection(desiredSection, this.currentSection, -105); // Call the global Mixin to scroll to desired section
+            this.showMenu = false;
         },
     },
 
