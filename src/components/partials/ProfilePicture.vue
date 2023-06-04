682d55ddecf6255a044ba525f3e7f5b9e8f1fd75
Derek Woo <me@derekw.co>
2023-06-03 23:15:06 -0700

    Disable draggability for profile picture

--- src/components/partials/ProfilePicture.vue
+++ src/components/partials/ProfilePicture.vue
@@ -1,3 +1,11 @@
 <template>
-    <img src="/assets/profile-picture.jpg" alt="Picture of Derek Woo" title="Display Picture" />
+    <img src="/assets/profile-picture.jpg" alt="Picture of Derek Woo" title="Display Picture" draggable="false" />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'ProfilePicture',
+});
+</script>
