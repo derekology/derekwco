1f01e25eb40a8e85447102f1bcb2ae38b045a079
Derek Woo <me@derekw.co>
2023-10-26 01:49:42 -0700

    Update skills section

--- src/components/sections/SkillSection.vue
+++ src/components/sections/SkillSection.vue
@@ -2,20 +2,20 @@
     <div id="skills-list">
         <span class="section-header">Development</span>
         <p>
-            HTML/CSS/JS/TS, EJS, Node.js, Express, Bootstrap, Vue, React, Google Firebase, Python
+            Python, Java, C, HTML/CSS/JS/TS, EJS, Node.js, Express, Bootstrap, Vue, React, Google Firebase
         </p>
         <span class="section-header">Automation / Scripting</span>
         <p>
-            Python, Javascript, Selenium, Twill, Bash, AMPScript, SSJS, Velocity
+            Python, JavaScript, Selenium, Twill, Bash, AMPScript, SSJS, Velocity
         </p>
         <span class="section-header">Databases</span>
         <p>
-            SQL, GraphQL, PostgreSQL, NoSQL, Google Firestore, MongoDB
+            SQL, Microsoft SQL Server, GraphQL, PostgreSQL, NoSQL, MongoDB, Google Firestore, Supabase
         </p>
-        <span class="section-header">Platforms</span>
+        <span class="section-header">Software / Platforms</span>
         <p>
-            Adobe Marketo Engage, Salesforce Marketing / Sales Cloud, MailChimp, MailerLite, Wix, Wordpress,
-            Manjaro, Arch Linux, Ubuntu, Windows
+            Git, Github, SourceTree, Wordpress, Linux, Windows, Adobe Marketo Engage, Salesforce Marketing / Sales Cloud,
+            MailChimp, MailerLite
         </p>
     </div>
 </template>
